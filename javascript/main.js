ee7ab4c48c6c969ecf8cc07b50e9ebac39a0af4f
Jorge Osvaldo Varela <94763025+osvaldovarela@users.noreply.github.com>
2024-02-29 22:38:55 -0300

    new update

---
+++ javascript/main.js
@@ -0,0 +1,32 @@
+// Obtener los elementos de la clase .close
+
+// Recorrerlos
+
+// Agregar un evento click a cada uno de ellos
+
+let links = document.querySelectorAll(".close");
+
+links.forEach(function (link) {
+  link.addEventListener("click", function (ev) {
+    // Código a ejecutar cuando se haga clic en el botón
+    ev.preventDefault();
+
+    let content = document.querySelector(".content");
+
+    // Quitarle las clases de animación que ya tiene
+
+    content.classList.remove("animate__fadeInDown");
+    content.classList.remove("animate__animated");
+
+    // Agregar clases para animar su salida fadeOutUp
+
+    content.classList.add("animate__fadeOutUp");
+    content.classList.add("animate__animated");
+
+    setTimeout(function () {
+      location.href = "/";
+    }, 600);
+
+    return false;
+  });
+});
